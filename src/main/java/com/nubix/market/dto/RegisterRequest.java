0b0f48d6aa154025c40165599a785b40b53d0f1e
DiegoAntonioCaceresAranda <u22212808@utp.edu.pe>
2026-05-20 22:27:35 -0500

    Añadido seguridad de contraseñas

--- src/main/java/com/nubix/market/dto/RegisterRequest.java
+++ src/main/java/com/nubix/market/dto/RegisterRequest.java
@@ -1,8 +1,23 @@
 package com.nubix.market.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
+
 public class RegisterRequest {
+
+    @NotBlank(message = "El username es obligatorio")
     private String username;
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Formato de email inválido")
     private String email;
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
+    @Pattern(
+        regexp ="^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[@$!%?&._#-])[A-Za-z\\d@$!%?&._#-]{8,}$",
+        message = "La contraseña debe tener al menos una mayúscula, una minúscula, un número y un carácter especial"
+    )
     private String password;
     
     public String getUsername() {return username;}
