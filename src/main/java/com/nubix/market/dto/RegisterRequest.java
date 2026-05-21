928784042a9318478e28ad83c3d731a3556c0d95
adilsoncarden <x.userprivate.2023@gmail.com>
2026-05-21 00:00:49 -0500

    Corrección de errores en security config

--- src/main/java/com/nubix/market/dto/RegisterRequest.java
+++ src/main/java/com/nubix/market/dto/RegisterRequest.java
@@ -14,16 +14,30 @@ public class RegisterRequest {
     private String email;
     @NotBlank(message = "La contraseña es obligatoria")
     @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
-    @Pattern(
-        regexp ="^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[@$!%?&._#-])[A-Za-z\\d@$!%?&._#-]{8,}$",
-        message = "La contraseña debe tener al menos una mayúscula, una minúscula, un número y un carácter especial"
-    )
+    @Pattern(regexp = "^^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&._#-])[A-Za-z\\d@$!%*?&._#-]{8,}$", message = "La contraseña debe tener al menos una mayúscula, una minúscula, un número y un carácter especial")
     private String password;
-    
-    public String getUsername() {return username;}
-    public void setUsername(String username) {this.username = username;}
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
-    public String getPassword() {return password;}
-    public void setPassword(String password) {this.password = password;}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
