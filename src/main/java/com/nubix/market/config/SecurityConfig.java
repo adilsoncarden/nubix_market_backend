a39b02cb3fdae5198639e87821c0fd7cd336169b
aiso01 <wilmertgl@gmail.com>
2026-05-20 22:05:38 -0500

    correeción de datos ventas
    ...

--- src/main/java/com/nubix/market/config/SecurityConfig.java
+++ src/main/java/com/nubix/market/config/SecurityConfig.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig {   
 
     @Autowired
     private JwtAuthFilter jwtAuthFilter;
