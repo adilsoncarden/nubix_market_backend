ed39bb19c391e176befafcc83e29bca59ced8a08
aiso01 <wilmertgl@gmail.com>
2026-05-21 22:04:34 -0500

    creación de clase GlobalExceptionHandler.java

--- src/main/java/com/nubix/market/config/SecurityConfig.java
+++ src/main/java/com/nubix/market/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // Usamos un patrón más limpio para rutas públicas
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/error").permitAll()  
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
